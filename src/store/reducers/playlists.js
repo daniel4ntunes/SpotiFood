d39312acb7127b642e9f1a6c8edc0aa603e6dbbe
Daniel Antunes <daniel.antunes@kroton.com.br>
2020-10-11 22:30:32 -0300

    :art: improve code style

--- src/store/reducers/playlists.js
+++ src/store/reducers/playlists.js
@@ -5,35 +5,32 @@ const INITIAL_STATE = {
 };
 
 const playlists = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case "LOADING_PLAYLISTS":
-      return {
-        ...state,
-        loading: true,
-        error: null,
-      };
-    case "SUCCESS_PLAYLISTS":
-      return {
-        ...state,
-        playlists: action.payload,
-        loading: false,
-        error: null,
-      };
-    case "FAILURE_PLAYLISTS":
-      return {
-        ...state,
-        playlists: [],
-        loading: false,
-        error: action.payload,
-      };
-    case "SEARCH_PLAYLISTS":
-      return {
-        ...state,
-        text: action.payload,
-      };
-    default:
-      return state;
-  }
+  const Users = {
+    LOADING_PLAYLISTS: {
+      ...state,
+      loading: true,
+      error: null,
+    },
+    SUCCESS_PLAYLISTS: {
+      ...state,
+      playlists: action.payload,
+      loading: false,
+      error: null,
+    },
+    FAILURE_PLAYLISTS: {
+      ...state,
+      playlists: [],
+      loading: false,
+      error: action.payload,
+    },
+    SEARCH_PLAYLISTS: {
+      ...state,
+      text: action.payload,
+    },
+    default: state,
+  };
+
+  return Users[action.type] || Users.default;
 };
 
 export default playlists;
